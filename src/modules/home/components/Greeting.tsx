f0f01d6d913cefe49717bb4a8b65756214e6901d
oshpraween <it22586384@my.sliit.lk>
2025-08-11 12:02:25 +0530

    remove night time

--- src/modules/home/components/Greeting.tsx
+++ src/modules/home/components/Greeting.tsx
@@ -12,8 +12,7 @@ const getGreeting = () => {
   const hour = new Date().getHours();
   if (hour >= 5 && hour < 12) return 'Good morning';
   if (hour >= 12 && hour < 18) return 'Good afternoon';
-  if (hour >= 18 && hour < 21) return 'Good evening';
-  return 'Good night';
+  return 'Good evening'; // Covers 18:00 → 5:00
 };
 
 const Greeting: React.FC<GreetingProps> = ({ name }) => {
